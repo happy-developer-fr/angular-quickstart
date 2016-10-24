de3661d919374dfedfc823665e42c4e96c875b04
COTTET Julien <tetto@gmail.com>
2016-10-24 21:55:27 +0200

    Correction du view providers

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,6 @@ import { userComponent } from './app.users.component'
 @Component({
   selector: 'my-app',
   template: `<h1>My First Angular App</h1><h2><users></users></h2>`,
-  directives: [userComponent]
+  viewProviders: [userComponent]
 })
 export class AppComponent { }
