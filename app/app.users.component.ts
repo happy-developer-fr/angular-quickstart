21dbd9564de0a32817c17deee6f22ca7a4b09d2b
COTTET Julien <tetto@gmail.com>
2016-10-24 22:18:52 +0200

    Ajout du premier bouton pour tester

--- app/app.users.component.ts
+++ app/app.users.component.ts
@@ -8,14 +8,20 @@ import {FamilleService} from "./app.famille.service"
                   {{user.name}}
                 </li>
               </ul>
-  `,
+              <button (click)="addMember()">Ajouter un prénom</button>`,
   providers:[FamilleService]
 })
 export class userComponent {
-  title = "La petite famille s'agrandit, trouvez le nom de notre petite future";
+  title = "La petite famille s'agrandit, essayez de trouver le prénom de notre petite future";
   users=[];
+
+  addMember(){
+    this.users.push({id:1,name:"new"})
+  }
 
   constructor(private _familleService:FamilleService){
     this.users=_familleService.getFamille();
   }
+
+
 }
